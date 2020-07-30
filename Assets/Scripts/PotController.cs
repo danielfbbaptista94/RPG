d50c2a582d535ae65fcca358a63c5ddd0a81d0f5
daniel <danielbaptista@smart4sistemas.com>
2020-07-29 21:25:26 -0300

    Add Player Attack and Pot

---
+++ Assets/Scripts/PotController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotController : MonoBehaviour
+{
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetSmash()
+    {
+        _animator.SetBool("smash", true);
+    }
+
+}
